9854a2d44f3f8571788cd4241c6309e12deb4354
Daurin Lora <daurinlora@gmail.com>
2019-11-21 21:15:01 -0600

    =no message

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ app.use((error, req, res, next) =>{
     else next();
 });
 app.use(cors({
-    origin:[process.env.CLIENT_URL,'http://localhost:3000']
+    origin:[process.env.CLIENT_URL,'*']
 }));
 
 // Routes
